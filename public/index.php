251311475b53973af8bc93fb92b08af2fabd918d
ivanmelo47 <ivanmelo47@gmail.com>
2024-05-02 05:47:24 -0600

    ss

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use Controllers\APIEventos;
+use Controllers\APIPonentes;
 use MVC\Router;
 use Controllers\AuthController;
 use Controllers\DashboardController;
@@ -57,6 +58,7 @@ $router->get('/admin/regalos', [RegalosController::class, 'index']);
 
 // API
 $router->get('/api/eventos-horario', [APIEventos::class, 'index']);
+$router->get('/api/ponentes', [APIPonentes::class, 'index']);
 
 
 $router->comprobarRutas();
